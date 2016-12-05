b10e03bec89f30f02c7b6550a5e01e30cbac2763
Cedric David <cedric.david@btopenworld.com>
2016-12-05 06:39:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  else
+    i = 2
+    while i < num
+      if num % i == 0
+        return false
+        break
+
+      end
+      i+=1
+    end
+
+  end
+return true
+
+end
