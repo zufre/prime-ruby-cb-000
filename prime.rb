4125969c39033b8da0ba776cda8c59bc5ab217e9
Cedric David <cedric.david@btopenworld.com>
2018-09-02 14:03:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,12 @@
 def prime?(num)
   if num == 2 || num == 3
     return true
+  elsif num == 4
+    return true
   end
-  range = (2..num-1).to_a
+  range = (3..num-1).to_a
   range.any? do |item|
-     num % item != 0 
+     num % item != 0
   end
 
 
