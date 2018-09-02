331eb23c92935c4f99206d73b1ec9620742beac4
Cedric David <cedric.david@btopenworld.com>
2018-09-02 14:11:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,13 @@
-# Add  code here!
-# def prime?(num)
-#   if num == 2 || num == 3
-#     return true
-#   elsif num <= 1 || num == 4
-#     return false
-#   end
-#   range = (3..num-1).to_a
-#   range.none? do |item|
-#      num % item == 0
-#   end
-#
-#
-#
-# end
-# Add  code here!
+
+def prime?(num)
+  if num == 2 || num == 3
+    return true
+  elsif num <= 1 || num == 4
+    return false
+  end
+  range = (3..num-1).to_a
+  range.none? do |item|
+     num % item == 0
+  end
+end
+
