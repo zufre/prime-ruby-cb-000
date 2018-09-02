d058a3606e825c4c8e0baecdba80b66359d010b4
Cedric David <cedric.david@btopenworld.com>
2018-09-02 14:11:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
-# Add  code here!
-def prime?(num)
-  range = (3..num-1).to_a
-  range.any? do |item|
-     num % item != 0
-  end
 
+def prime?(num)
+  if num == 2 || num == 3
+    return true
+  elsif num <= 1 || num == 4
+    return false
+  end
+  range = (3..num-1).to_a
+  range.none? do |item|
+     num % item == 0
+  end
 end
